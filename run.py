b6dd4f1403a07e41e28d35d206455ee26f100ea3
DarkUlixes <reiarifi@gmail.com>
2020-02-29 23:42:05 +0100

    dont need init

--- run.py
+++ run.py
@@ -1,14 +1,4 @@
 from eq import app
 from instance.config import *
 
-
-def init():
-    mysql_db = MySQLDatabase('earthquakedb', user='root', password='my-secret-pw', host=MYSQL_IP, port=MYSQL_PORT)
-    print(mysql_db.connection())
-
-
-if __name__ == '__main__':
-    init()
-
-
 app.run(debug=True)
