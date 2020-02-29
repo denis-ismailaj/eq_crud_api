d0ab462c68315f29ce11abd89683e6622cfd4c92
Denis Ismailaj <20902736+denis-ismailaj@users.noreply.github.com>
2020-02-29 18:48:54 +0100

    Connected to DB

--- eq/__init__.py
+++ eq/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from peewee import *
+from instance.config import *
 
 app = Flask(__name__)
-# app.config.from_object('config')
-# app.config.from_file('config.py')
+app.config.from_pyfile("../instance/config.py")
+
 
 @app.route('/')
 def say_hello():
     return "Hello world"
+
--- run.py
+++ run.py
@@ -1,3 +1,15 @@
 from eq import app
+from peewee import *
+from instance.config import *
 
-app.run( debug = True )
+
+def init():
+    mysql_db = MySQLDatabase('earthquakedb', user='root', password='my-secret-pw', host=MYSQL_IP, port=MYSQL_PORT)
+    print(mysql_db.connection())
+
+
+if __name__ == '__main__':
+    init()
+
+
+app.run(debug=True)
