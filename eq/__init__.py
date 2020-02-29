116feff04d550495ec071f124bfb13dcf2c26029
Hysen Ndr/egjoni <hndregjoni@hotmail.com>
2020-02-29 22:27:05 +0100

    Entities are ready. Connection to db works.

--- eq/__init__.py
+++ eq/__init__.py
@@ -2,11 +2,26 @@ from flask import Flask
 from peewee import *
 from instance.config import *
 
+from eq.model import database, Earthquake
+
+from playhouse.shortcuts import model_to_dict
+
 app = Flask(__name__)
 app.config.from_pyfile("../instance/config.py")
 
+@app.before_request
+def before_request():
+    database.connect()
+
+@app.after_request
+def after_request():
+    database.close()
+    return response
 
 @app.route('/')
 def say_hello():
     return "Hello world"
 
+@app.route('/api/eq')
+def get_earthquake():
+    Earthquake.select()
---
+++ eq/model/__init__.py
@@ -0,0 +1,28 @@
+import json
+from peewee import *
+from instance.config import *
+
+from playhouse.shortcuts import model_to_dict
+
+# This is where the database goes
+database = MySQLDatabase('earthquakedb', user='root', password='my-secret-pw', host=MYSQL_IP, port=MYSQL_PORT)
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+    def stringify():
+        return json.dumps(model_to_dict(self)) 
+
+class Earthquake(BaseModel):
+    class Meta:
+        table_name = "earthquakes"
+
+    id = PrimaryKeyField(unique = True)
+    date = DateTimeField(db_column = "data")
+    time = DateTimeField(db_column = "ora")
+    latitude = DoubleField(db_column = "lattitude")
+    longitude = DoubleField(db_column = "longtitude")
+    depth = DoubleField(db_column = "thellesia")
+    distance = DoubleField(db_column = "largesia")
+    place = CharField(db_column = "vendodhja") 
